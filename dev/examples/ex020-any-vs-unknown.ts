031aa311f1fffdd1f22db52bea4e8b0a820f7641
Edward Tanguay (windows) <edward@tanguay.info>
2025-07-04 17:14:25 +0200

    CONTENT: updated 020

--- dev/examples/ex020-any-vs-unknown.ts
+++ dev/examples/ex020-any-vs-unknown.ts
@@ -2,6 +2,6 @@ let data1 = "hello";
 let data2:any = "hello";
 let data3:unknown = "hello";
 
-let info1:number = data1
+let info1:number = data1 // type error
 let info2:number = 24;
-let info3: number = data3;
+let info3:number = data3; // type error
